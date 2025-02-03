3358fb206c077827814a2007fd4b5bd9ec03e35a
jsuhaan <haanjisu@gmail.com>
2025-02-03 16:41:03 +0900

    [20250203] 사진 업로드 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,42 @@ import Postdata from './data/Postdata.js'
 import axios from 'axios'
 import { ref } from 'vue';
 
+const buttonCount = ref(0);
+const step = ref(0);
+const imageUrl = ref('');
+
 // 게시물 데이터
-const posts = ref(Postdata);
+const posts = ref(Postdata); // Postdata에 새로운 데이터 추가됐을 때, 자동으로 템플릿에 반영되도록 ref로 선언.
 
 // 메서드
+// 1) 포스트 더보기 버튼
 const showMore = () => {
-  axios.get('https://codingapple1.github.io/vue/more0.json')
+  axios.get(`https://codingapple1.github.io/vue/more${buttonCount.value}.json`)
+  // 템플릿 리터럴(` 백틱) : 변수를 ${} 구문을 통해 문자열 안에 포함시키고 싶을 때에 사용. 일반 따옴표로 처리하면 x.
     .then(response => {
+      buttonCount.value++;
       posts.value.push(response.data);
     })
     .catch(error => {
       console.error("데이터를 불러오는 중 오류 발생:", error);
     });
+};
+// 2) 사진 업로드
+const upload = (e) => {
+  let file = e.target.files[0];
+  // e.target.files[0]은 Blob 객체. 
+  // *Blob은 미디어 파일이나, 파일로서 저장될 수 있는 데이터를 표현할 때 사용. 사용자가 선택한 파일은 이미 Blob 객체 형태로 존재
+
+  // 파일 확장자 확인 (이미지 파일만 허용)
+  const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+  const fileExtension = file.name.split('.').pop().toLowerCase(); // 파일 확장자 추출
+  if (allowedExtensions.includes(`.${fileExtension}`)){
+    imageUrl.value = URL.createObjectURL(file); 
+    step.value++;
+  } else {
+    alert("이미지 파일만 업로드 가능합니다."); // 이미지가 아닌 경우 경고
+  }
+
 };
 </script>
 
@@ -30,16 +54,18 @@ const showMore = () => {
     <img src="./assets/logo.svg" class="logo" />
   </div>
 
-  <Container :posts="posts" />
+  <Container :posts="posts" :step="step" :imageUrl="imageUrl" />
   <button class="show-button" @click="showMore">
     더보기
   </button>
 
-  <div class="footer">
-    <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
-      <label for="file" class="input-plus">+</label>
-    </ul>
+  <div class="flex-container" v-if="step==0">
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input @change="upload" type="file" id="file" class="inputfile" /> <!-- id="file"은 <label>과 연결되기 위해 사용 -->
+        <label for="file" class="input-plus">+</label> <!-- input창은 가리고, 라벨만 보여준다. -->
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -56,5 +82,9 @@ const showMore = () => {
   background-color: white;
   color: #191919;
   border: 1px solid #191919;
+}
+.flex-container{
+  display: flex;
+  justify-content: center;
 }
 </style>
