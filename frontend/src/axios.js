b597dcce7ab3a8ed30da09dbb6fc314cb2b2a2a7
hanjisu <haanjisu@gmail.com>
2025-02-24 16:30:10 +0900

    FIX - [20250224] 포트 번호 변경

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/',  // Spring Boot 서버 URL
+  baseURL: 'http://localhost:8082/api/',  // Spring Boot 서버 URL
   timeout: 10000,  // 요청 타임아웃 시간 설정
   headers: {
     'Content-Type': 'application/json',
